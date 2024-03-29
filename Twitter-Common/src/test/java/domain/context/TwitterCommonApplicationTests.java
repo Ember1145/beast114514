b0154b51eb0f0fcdea29c629c738154c82032916
LTS <14195514+beast-ancestors-114@user.noreply.gitee.com>
2024-03-29 17:32:37 +0800

    start

---
+++ Twitter-Common/src/test/java/domain/context/TwitterCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package domain.context;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Twitter-pojo/src/test/java/domain/twitterpojo/TwitterPojoApplicationTests.java
@@ -0,0 +1,13 @@
+package domain.twitterpojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterPojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
